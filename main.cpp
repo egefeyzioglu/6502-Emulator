f730723caaaa7874dd7246fe37eba62805840e3d
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2022-09-21 19:18:29 -0400

    Clean up unused defines

--- main.cpp
+++ main.cpp
@@ -11,18 +11,6 @@
 #include "emulator.h"
 #include "mainwindow.h"
 
-#define CEIL_DIVIDE_INT(x, y) x / y + (x % y > 0)
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0')
-
 // The emulator instance
 Emulator *emulator;
 
